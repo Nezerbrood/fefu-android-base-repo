8967431f018058d5cebdb5ed5d350d3cbb7ba004
Gagaved <Nezerbrood@protonmail.com>
2022-01-28 05:11:56 +1000

    homework7

--- app/src/main/java/ru/fefu/activitytracker/fragments/ChangePassFragment.kt
+++ app/src/main/java/ru/fefu/activitytracker/fragments/ChangePassFragment.kt
@@ -1,44 +1,45 @@
 package ru.fefu.activitytracker.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
-import ru.fefu.activitytracker.R
 import ru.fefu.activitytracker.databinding.FragmentChangePassBinding
 
-class ChangePassFragment : Fragment() {
+class ChangePassFragment: Fragment() {
     private var _binding: FragmentChangePassBinding? = null
     private val binding get() = _binding!!
+
+    companion object {
+        fun newInstance(): ChangePassFragment {
+            return ChangePassFragment()
+        }
+        const val tag = "profile_change"
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentChangePassBinding.inflate(inflater, container, false)
         return binding.root
     }
-    companion object{
-        fun newInstance(message: String): ProfileFragment {
-            val args = Bundle()
-            args.putString("message", message)
-            val fragment = ProfileFragment()
-            fragment.arguments = args
-            return fragment
-        }
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.toolbar.setNavigationOnClickListener {
+        binding.toolbar.setNavigationOnClickListener{
             findNavController().popBackStack()
         }
         binding.buttonContinue.setOnClickListener{
-            // TODO: 12/11/2021 implement password change
+            // TODO: 1/28/2022 Нет api для смены пароля 
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/ru/fefu/activitytracker/fragments/ProfileFragment.kt
+++ app/src/main/java/ru/fefu/activitytracker/fragments/ProfileFragment.kt
@@ -1,43 +1,86 @@
 package ru.fefu.activitytracker.fragments
-
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import ru.fefu.activitytracker.App
 import ru.fefu.activitytracker.R
+import ru.fefu.activitytracker.activities.WelcomeActivity
 import ru.fefu.activitytracker.databinding.FragmentProfileBinding
-class ProfileFragment : Fragment() {
+import ru.fefu.activitytracker.retrofit.Result
+import ru.fefu.activitytracker.retrofit.response.UserModel
+
+class ProfileFragment: Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+    private lateinit var viewModel: ProfileViewModel
+
+    companion object {
+        fun newInstance(): ProfileFragment {
+            return ProfileFragment()
+        }
+        const val tag = "profile_view"
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        viewModel = ViewModelProvider(this)[ProfileViewModel::class.java]
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
-    }
-    companion object{
-            fun newInstance(message: String): ProfileFragment {
-            val args = Bundle()
-            args.putString("message", message)
-            val fragment = ProfileFragment()
-            fragment.arguments = args
-            return fragment
-        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.change.setOnClickListener {
+
+        binding.change.setOnClickListener{
             findNavController().navigate(R.id.action_profileFragment_to_changePassFragment)
         }
-        binding.saveBtn.setOnClickListener{
-            // TODO: 12/11/2021 implement profile saving
+
+        viewModel.logoutUser
+            .onEach {
+                if (it is Result.Success<Unit>) {
+                    App.INSTANCE.sharedPrefs.edit().remove("token").apply()
+                    val intent = Intent(requireActivity(), WelcomeActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                }
+                else if (it is Result.Error<Unit>) {
+                    Toast.makeText(requireContext(), it.e.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
+            .launchIn(lifecycleScope)
+
+        viewModel.profile
+            .onEach {
+                if (it is Result.Success<UserModel>) {
+                    binding.login.editText?.setText(it.result.login)
+                    binding.nickname.editText?.setText(it.result.name)
+                }
+                else if (it is Result.Error<UserModel>) {
+                    Toast.makeText(requireContext(), it.e.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
+            .launchIn(lifecycleScope)
+        viewModel.getProfile()
+
+        binding.buttonLogout.setOnClickListener {
+            viewModel.logout()
         }
-        binding.buttonLogout.setOnClickListener{
-            // TODO: 12/11/2021 implement logout
-        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
